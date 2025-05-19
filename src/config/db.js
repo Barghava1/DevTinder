e67e8f889f4a21c47206b10caf1548b6eac825df
Barghava <barghavaramudu537@gmail.com>
2025-05-19 19:58:02 +0530

    db

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 require("dotenv").config();
 
 const connectDB= async ()=>{
-    await mongoose.connect("mongodb+srv://barghavachitturi179:barghava123@barghavanode.skqvt7d.mongodb.net/devTinder")
+    await mongoose.connect(process.env.MONGODB_URL)
 }
 
 module.exports=connectDB;
